a5ae6eac3350f5faa8ac8f05781eea56683cb6be
stivin-00 <68236676+stivin-00@users.noreply.github.com>
2022-07-16 15:18:31 +0100

    changed module exports

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = nextConfig
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        DB_URL:
+          "mongodb+srv://stivin:vivian1234@eflix.xlz50.mongodb.net/todo?retryWrites=true&w=majority",
+      },
+    };
+  }
+
+  return {
+    env: {
+      DB_URL:
+        "mongodb+srv://stivin:vivian1234@eflix.xlz50.mongodb.net/todo?retryWrites=true&w=majority",
+    },
+  };
+};
